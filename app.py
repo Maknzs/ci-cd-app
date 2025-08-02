864eadbad540f8925dde308f1b48e18607220ded
Mak Snodgrass <mak@snodgrassenterprise.com>
2025-08-01 20:18:51 -0400

    Add counter button template and route

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
+count = 0
 
 @app.route('/')
 def hello():
     return "Hello, World!"
+
+@app.route('/counter', methods=['GET', 'POST'])
+def counter():
+    global count
+    if request.method == 'POST':
+        count += 1
+    return render_template('counter.html', count=count)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
